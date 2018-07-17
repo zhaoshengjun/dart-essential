959dc5ff0de2277a1739509985369d2db44bc7f4
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-17 22:46:58 +1000

    fuzzy search

--- lib/src/fuzzy_search.dart
+++ lib/src/fuzzy_search.dart
@@ -1,3 +1,38 @@
 part of fuzzy;
 
-class FuzzySearch {}
+class FuzzySearch {
+  List<String> list;
+
+  FuzzySearch(this.list);
+
+  List<String> search(String term) {
+    List<String> results = [];
+    if (term.isEmpty) {
+      return [];
+    }
+
+    List<String> result = list.where((String key) {
+      int ti = 0;
+
+      for (int si = 0; si < key.length; si++) {
+        if (term[ti] == key[si]) {
+          ti++;
+          if (ti == term.length) {
+            return true;
+          }
+        }
+        return false;
+      }
+    }).toList(growable: false);
+
+    result.sort((String a, String b) {
+      if (a.length > b.length) {
+        return 1;
+      } else if (a.length == b.length) {
+        return 0;
+      }
+      return -1;
+    });
+    return result;
+  }
+}
