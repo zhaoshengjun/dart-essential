4620a99144c3f262b92fbb6cdc22e6107f589ada
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-17 22:58:34 +1000

    update main

--- web/main.dart
+++ web/main.dart
@@ -1,3 +1,24 @@
 import 'package:dart_essential/fuzzy.dart';
+import 'dart:html';
+import 'dart:convert';
+import 'dart:async';
 
-var obj = new FuzzySearch();
+class DocSearch {
+  static fromJson(Element root, String url) async {
+    String json = await HttpRequest.getString(url);
+    Map decoded = JSON.decode(json);
+    return new DocSearch(root, decoded);
+  }
+
+  DocSearch(Element root, [Map<String, dynamic> inputDict]) {}
+}
+
+main() async {
+  try {
+    await DocSearch.fromJson(querySelector('body'), 'dict.json');
+  } catch (e) {
+    print("It's broken");
+  }
+}
+
+// var obj = new FuzzySearch();
