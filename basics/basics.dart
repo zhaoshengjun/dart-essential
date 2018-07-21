cc46d31dcb87b294306395f4d6961c3d99341fc2
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-21 22:54:22 +1000

    static method

--- basics/basics.dart
+++ basics/basics.dart
@@ -1,4 +1,6 @@
 // class
+import 'dart:math';
+
 class Point {
   var x, y;
   Point(this.x, this.y);
@@ -6,10 +8,16 @@ class Point {
   scale(factor) => new Point(x * factor, y * factor);
 
   operator +(p) => new Point(x + p.x, y + p.y);
+
+  static distance(p1, p2) {
+    var dx = p1.x - p2.x;
+    var dy = p1.y - p2.y;
+    return sqrt(dx * dx + dy * dy);
+  }
 }
 
 void main() {
   var aPoint = new Point(5, 1);
   var bPoint = new Point(3, 4);
-  print((aPoint + bPoint).x);
+  print(Point.distance(aPoint, bPoint));
 }
