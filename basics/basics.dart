b8cdfbb66a80420d8a5eb5a1fc6d363db15d9354
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-22 22:36:43 +1000

    initializer list

--- basics/basics.dart
+++ basics/basics.dart
@@ -1,19 +1,27 @@
+import 'dart:math';
+
 class Point {
-  var x, y;
-  Point(this.x, this.y);
-}
+  final rho, theta;
 
-class Point3D extends Point {
-  var z;
+  Point(a, b)
+      : rho = sqrt(a * a + b * b),
+        theta = atan(a / b);
 
-  Point3D(a, b, c) : super(a, b) {
-    z = c;
+  get x => rho * cos(theta);
+  get y => rho * sin(theta);
+  scale(factor) => new Point(x * factor, y * factor);
+  operator +(p) => new Point(x + p.x, y + p.y);
+
+  static distance(p1, p2) {
+    var dx = p1.x - p2.x;
+    var dy = p1.y - p2.y;
+    return sqrt(dx * dx + dy * dy);
   }
 }
 
 void main() {
   var pointA = new Point(3, 4);
-  var pointB = new Point(3, 4);
-
-  print(pointA == pointB); // return true
+  var pointB = pointA.scale(5);
+  print(pointA.x);
+  print(pointB.x);
 }
