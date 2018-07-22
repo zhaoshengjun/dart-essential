41216082a5aab6eed8e8cfeab1146b24e0557d68
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-22 22:11:53 +1000

    override == for equality check

--- basics/basics.dart
+++ basics/basics.dart
@@ -2,11 +2,13 @@ class Point {
   var x, y;
 
   Point(this.x, this.y);
+
+  operator ==(p) => x == p.x && y == p.y;
 }
 
 void main() {
   var pointA = new Point(3, 4);
   var pointB = new Point(3, 4);
 
-  print(pointA == pointB); // return false
+  print(pointA == pointB); // return true
 }
