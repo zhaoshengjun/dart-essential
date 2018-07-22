b8b5f9f5ec267f1e9383f4092d8e29949cd3c312
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-22 22:02:51 +1000

    setter method

--- basics/basics.dart
+++ basics/basics.dart
@@ -5,6 +5,16 @@ class Point {
   Point(this.rho, this.theta);
   get x => rho * cos(theta);
   get y => rho * sin(theta);
+  set x(newX) {
+    rho = sqrt(newX * newX + y * y);
+    theta = acos(newX / rho);
+  }
+
+  set y(newY) {
+    rho = sqrt(x * x + newY * newY);
+    theta = asin(newY / rho);
+  }
+
   scale(factor) => new Point(rho * factor, theta);
   operator +(p) => new Point(x() + p.x(), y() + p.y());
   static distance(p1, p2) {
