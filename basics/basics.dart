8154c8235df8450992c9d6b29b910f5fb5166d36
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-22 22:10:43 +1000

    equality check

--- basics/basics.dart
+++ basics/basics.dart
@@ -1,6 +1,12 @@
-class Box {
-  static var numberOfInstance = 0;
-  Box() {
-    numberOfInstance = numberOfInstance + 1;
-  }
+class Point {
+  var x, y;
+
+  Point(this.x, this.y);
+}
+
+void main() {
+  var pointA = new Point(3, 4);
+  var pointB = new Point(3, 4);
+
+  print(pointA == pointB); // return false
 }
