d2f084a5598969057f508927cf8bd3502ad7aab6
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-22 21:59:46 +1000

    init version for class to fix

--- basics/basics.dart
+++ basics/basics.dart
@@ -1,19 +1,15 @@
-// library
-library points;
-
 import 'dart:math';
 
 class Point {
-  var x, y;
-  Point(this.x, this.y);
-
-  scale(factor) => new Point(x * factor, y * factor);
-
-  operator +(p) => new Point(x + p.x, y + p.y);
-
+  var rho, theta;
+  Point(this.rho, this.theta);
+  x() => rho * cos(theta);
+  y() => rho * sin(theta);
+  scale(factor) => new Point(rho * factor, theta);
+  operator +(p) => new Point(x() + p.x(), y() + p.y());
   static distance(p1, p2) {
-    var dx = p1.x - p2.x;
-    var dy = p1.y - p2.y;
+    var dx = p1.x() - p2.x();
+    var dy = p1.y() - p2.y();
     return sqrt(dx * dx + dy * dy);
   }
 }
