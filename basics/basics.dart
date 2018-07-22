287455cd8bfc5208f37d67bd4cb7d074a5d1519e
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-22 22:05:28 +1000

    static method

--- basics/basics.dart
+++ basics/basics.dart
@@ -1,28 +1,6 @@
-import 'dart:math';
-
-class Point {
-  var rho, theta;
-  Point(a, b) {
-    rho = sqrt(a * a + b * b);
-    theta = atan(a / b);
-  }
-  get x => rho * cos(theta);
-  get y => rho * sin(theta);
-  set x(newX) {
-    rho = sqrt(newX * newX + y * y);
-    theta = acos(newX / rho);
-  }
-
-  set y(newY) {
-    rho = sqrt(x * x + newY * newY);
-    theta = asin(newY / rho);
-  }
-
-  scale(factor) => new Point(rho * factor, theta);
-  operator +(p) => new Point(x() + p.x(), y() + p.y());
-  static distance(p1, p2) {
-    var dx = p1.x() - p2.x();
-    var dy = p1.y() - p2.y();
-    return sqrt(dx * dx + dy * dy);
+class Box {
+  static var numberOfInstance = 0;
+  Box() {
+    numberOfInstance = numberOfInstance + 1;
   }
 }
