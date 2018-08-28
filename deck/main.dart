276450e0af7190f9316a7a4f1f1c45228c6291ad
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-28 22:50:25 +1000

    create deck

--- deck/main.dart
+++ deck/main.dart
@@ -1,3 +1,42 @@
-class Cards {}
+enum Rank {
+  Ace,
+  Jack,
+  Queen,
+  King,
+  One,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Night,
+  Ten
+}
+enum Suit { Diamond, Heart, Club, Spade }
 
-class Deck {}
+class Card {
+  Suit suit;
+  Rank rank;
+
+  Card(this.suit, this.rank);
+}
+
+class Deck {
+  List<Card> cards;
+
+  Deck() {
+    Suit.values.forEach((s) {
+      Rank.values.forEach((r) {
+        cards.add(new Card(s, r));
+      });
+    });
+  }
+
+  printCards() {}
+  shuffle() {}
+
+  deal() {}
+  removeCard() {}
+}
